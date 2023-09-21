b246469423460953f48846bd2d02a6f8d9bf25c8
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-09-21 19:42:47 +0200

    ya funciona compiland con readline

--- execute/signals.c
+++ execute/signals.c
@@ -6,7 +6,7 @@
 /*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 17:00:07 by crtorres          #+#    #+#             */
-/*   Updated: 2023/09/19 18:52:02 by crtorres         ###   ########.fr       */
+/*   Updated: 2023/09/21 19:31:52 by crtorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 void	signal_input(int sig)
 {
-	(void) sig;
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	if (sig == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 void	handle_sign(void)
 {
-	signal(SIGINT, &signal_input);
 	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, &signal_input);
 }
