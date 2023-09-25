87dc9ba27cdd874726fec06faa684f5e8c93567b
dlopez-s <dlopez-s@student.42.com>
2023-09-25 16:26:57 +0200

    peta ctrl+D

--- execute/main.c
+++ execute/main.c
@@ -6,7 +6,7 @@
 /*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 15:10:39 by crtorres          #+#    #+#             */
-/*   Updated: 2023/09/22 10:58:57 by dlopez-s         ###   ########.fr       */
+/*   Updated: 2023/09/25 15:45:10 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	main(int argc, char **argv, char **envp)
 	(void) argv;
 
 	tokens = NULL;
-	line = "";		//inicializacion a lo guarro
+	line = ft_strdup("");		//inicializacion a lo guarro
 	data = ft_calloc(1, sizeof(t_data));
 	data->envi = envp;
 	data->env_copy = ft_calloc(ft_matrix_len(envp) + 1, sizeof(char *));
@@ -64,7 +64,7 @@ int	main(int argc, char **argv, char **envp)
 		}
 		else
 			ft_execute(tokens, data);
-		// free(tokens);
+		// free(tokens); 
 		// printf("Line: %s\n", line);
 	}
 	return (0);
