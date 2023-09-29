ebf22df254223cf09bf8bfff8b61a5f2b3433f70
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-09-29 15:03:42 +0200

    pruebas

--- parsing/utils.c
+++ parsing/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 00:00:26 by lopezz            #+#    #+#             */
-/*   Updated: 2023/09/26 15:57:27 by crtorres         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:52:33 by crtorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void read_list(t_token *cmd_lst)
 	printf("\n");
 }
 
-t_token	*add_token(t_token *cmd_lst, char *cmd, int type, int is_quoted)
+t_token	*add_token(t_token *cmd_lst, char *cmd, int type)
 {
 	t_token	*new;
 	t_token	*aux;
@@ -56,7 +56,6 @@ t_token	*add_token(t_token *cmd_lst, char *cmd, int type, int is_quoted)
 		printf("entra\n");
 		new->args = ft_split(cmd, ' ');
 	}
-	
 	new->args = ft_split(cmd, ' ');
 	new->type = type;
 	new->next = NULL;
@@ -71,6 +70,73 @@ t_token	*add_token(t_token *cmd_lst, char *cmd, int type, int is_quoted)
 	}
 	return (cmd_lst);
 }
+char	*ft_quote(char *str)
+{
+	int		i;
+	int		r;
+	int		quote;
+	char	*new;
+	char	*ret;
+
+	i = 0;
+	r = 0;
+	new = malloc(ft_strlen(str) + 1);
+	while (str[i])
+	{
+		while (str[i] && str[i] == SQUOTE || str[i] == DQUOTE)
+		{
+			quote = str[i++];
+			while (str[i] && str[i] != quote)
+			{
+				if (ft_strchr("\t\v\n >|<", str[i]))
+					str[i] = -str[i];
+				new[r++] = str[i++];
+			}
+			i++;
+		}
+		if (!str[i])
+			break;
+		new[r++] = str[i++];
+	}
+	new[r] = 0;
+	ret = ft_strdup(new);
+	free_all(2, 0, & new, & str);
+	return (ret);
+}
+/* int 	check_close_quotes(char *cmd)
+{
+	char 	c;
+	int		quotes;
+	int		i;
+	
+	quotes = 0;
+	i = 0;
+	if (!cmd)
+		return (0);
+	while (cmd[i])
+	{	
+		while (cmd[i] && (cmd[i] != '"' || cmd[i] != '\''))
+			i++;
+		if (cmd[i] && (cmd[i] == '"' || cmd[i] == '\''))
+		{
+			c = cmd[i];
+			if (cmd[i++] == c)
+				quotes = cmd[i];
+			while (cmd[i] && cmd[i] != c)
+				i++;
+			if (cmd[i++] == c)
+			quotes++;
+			else 
+				break;
+		}
+	}
+	if (quotes % 2)
+	{
+		error_msg("unclosed quotes");
+		free (cmd);
+	}
+	return (0);
+} */
 
 bool is_operator(char c)
 {
