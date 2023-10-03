57e9577e5c042b37d8ef6d5e9017c9ef0e6f7205
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-10-03 16:49:25 +0200

    funcion de init dolar acabada

--- execute/expand.c
+++ execute/expand.c
@@ -6,7 +6,7 @@
 /*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 12:07:15 by crtorres          #+#    #+#             */
-/*   Updated: 2023/10/03 16:35:29 by crtorres         ###   ########.fr       */
+/*   Updated: 2023/10/03 16:48:40 by crtorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,23 @@ int	check_init_dollar(char *str, int *len, char *string, t_data *env)
 	new = get_env(str, env);
 	if (!*s)
 	{
-		ft_strcat
+		ft_strcat(string, "$");
+		*len += 1;
 	}
+	if (new)
+	{
+		*len += ft_strlen(new);
+		ft_strcat(string, new);
+	}
+	free (new);
+	free (str);
+	return (i);
 }
 
 int	expandlen(char *str, t_data *env)
 {
 	int	i;
 	int	len;
-<<<<<<< HEAD
 
 	i = 0;
 	len = 0;
@@ -59,6 +67,15 @@ int	expandlen(char *str, t_data *env)
 			i += check_init_dollar(str[i], &len, NULL, env);
 		if (str[i] == '\0')
 			break ;
+		if (str[i] && str[i] == SINGLE_QUOTES)
+		{
+			i++;
+			len++;
+			while (str[i] && str[i] != SINGLE_QUOTES)
+			{
+				
+			}
+		}
 	}
 }
 
@@ -71,7 +88,6 @@ char ft_expand(char *str, t_data *env)
 
 	len = expandlen(str, env);
 	str = ft_calloc(len +1, 1);
-=======
 	
 	i = 0;
 	len = 0;
