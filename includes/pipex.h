ae2b6d319cd5c88d055b389ef84226c71a7aa1a9
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-12-04 12:56:34 +0100

    tmp_hd in heredoc

--- includes/pipex.h
+++ includes/pipex.h
@@ -12,7 +12,6 @@ typedef struct s_token t_token;
 void	exec_cmd(t_token *tokens, char **env);
 int		find_path_pos(char **env);
 char	*find_in_path(char *cmd, char **env);
-void	find_path(char *cmd, char **env);
 int		error_found(char *str);
 void	free_mtx(char **mtx);
 int		ft_open(char *file, int check);
