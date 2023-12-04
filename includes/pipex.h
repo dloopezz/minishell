9c2e9d18e64703271ef4b32cd7a3da7e2a0bc4eb
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-12-04 18:02:25 +0100

    sigue sin funcionar

--- includes/pipex.h
+++ includes/pipex.h
@@ -11,7 +11,6 @@ typedef struct s_token t_token;
 //void	pipex(t_token *tokens, char *env[]);
 void	exec_cmd(t_token *tokens, char **env);
 int		find_path_pos(char **env);
-char	*find_in_path(char *cmd, char **env);
 int		error_found(char *str);
 void	free_mtx(char **mtx);
 int		ft_open(char *file, int check);
